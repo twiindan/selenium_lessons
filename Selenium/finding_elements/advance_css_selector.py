b2ac448902f60eef9f1cab401f8cd0cc0a1ddc01
Antonio Robres <arobres@Antonios-MacBook-Pro.local>
2017-10-14 23:49:32 +0200

    add xpath advances selectors class

--- Selenium/finding_elements/advance_css_selector.py
+++ Selenium/finding_elements/advance_css_selector.py
@@ -9,7 +9,7 @@ driver.get(baseUrl)
 
 '''
 One method to find resources in DOM is using CSS Selectors. The idea is use different techniques
-and tips to obtain a unique element in the webpage.
+and tips to obtain a unique element in the webpage using CSS selectors.
 '''
 
 '''Syntax for CSS Selectors
@@ -71,3 +71,67 @@ print(elementByCss.get_attribute('name'))
 
 elementByCss = driver.find_element_by_css_selector(".btn-style.class1.class2")
 print(elementByCss.get_attribute('href'))
+
+
+'''
+Is Possible is wildcards inside the CSS Selectors using the following syntax:
+
+tag[attribute<special-character>=value]
+
+The special characters used as wildcards are the following:
+
+    - Represents the starting text --> ^
+    - Represents the ending text --> $
+    - Represents the text contained
+'''
+
+
+'''
+Examples using wildcards
+
+input[class='inputs'] --> 1 matching element
+input[class^='inputs'] --> 2 matching element
+input[class$='inputs'] --> 1 matching element
+input[class*='inputs'] --> 2 matching element
+
+'''
+
+elementsByCss = driver.find_elements_by_css_selector("input[class='inputs']")
+print(len(elementsByCss))
+
+elementsByCss = driver.find_elements_by_css_selector("input[class^='inputs']")
+print(len(elementsByCss))
+
+elementsByCss = driver.find_elements_by_css_selector("input[class$='inputs']")
+print(len(elementsByCss))
+
+elementsByCss = driver.find_elements_by_css_selector("input[class*='inputs']")
+print(len(elementsByCss))
+
+
+'''
+Is possible also find the elements finding his children nodes using the character ">".
+
+fieldset>table
+fieldset>#product
+fieldset>button
+fieldset>input#name
+
+
+'''
+
+elementByCss = driver.find_element_by_css_selector("fieldset>table")
+print(elementByCss.get_attribute('name'))
+
+
+elementByCss = driver.find_element_by_css_selector("fieldset>#product")
+print(elementByCss.get_attribute('name'))
+
+
+elementByCss = driver.find_element_by_css_selector("fieldset>button")
+print(elementByCss.get_attribute('name'))
+
+
+elementByCss = driver.find_element_by_css_selector("fieldset>input#name")
+print(elementByCss.get_attribute('name'))
+
