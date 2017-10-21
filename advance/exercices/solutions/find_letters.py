6048e1823f0e84a50549e3f161256c1448336512
Antonio Robres <arobres@Antonios-MacBook-Pro.local>
2017-10-21 16:31:26 +0200

    count characters exercice

---
+++ advance/exercices/solutions/find_letters.py
@@ -0,0 +1,19 @@
+
+# Find the number of letters "a" or "A" in a string using control flow
+
+counter = 0
+
+string = 'Python is a widely used high-level programming language for general-purpose programming, ' \
+         'created by Guido van Rossum and first released in 1991. An interpreted language, Python has a design ' \
+         'philosophy that emphasizes code readability (notably using whitespace indentation to delimit code blocks ' \
+         'rather than curly brackets or keywords), and a syntax that allows programmers to express concepts in fewer ' \
+         'lines of code than might be used in languages such as C++ or Java.[23][24] ' \
+         'The language provides constructs intended to enable writing clear programs on both a small and large scale.'
+
+# Iterate the string character by character
+
+
+# Compare if character is "a" or "A"
+
+
+print(counter)
