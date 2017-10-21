32ddbee9d9fd9c94cd5066ea40c4951c557b0e51
Toni Robres <twiindan@gmail.com>
2017-10-22 00:12:40 +0200

    fix switch to iframe test

--- Selenium/switch_window_iframe/switch_to_iframe.py
+++ Selenium/switch_window_iframe/switch_to_iframe.py
@@ -3,21 +3,18 @@ from selenium.webdriver.common.by import By
 import time
 
 
-baseUrl = "http://www.mclibre.org/consultar/htmlcss/html/html_objetos.html#iframe"
+baseUrl = "https://jqueryui.com/button/"
 driver = webdriver.Firefox()
 driver.maximize_window()
 driver.get(baseUrl)
 
-# Switch to frame using numbers
-driver.switch_to.frame(0)
-
 # Switch to frame using element
-iframe = driver.find_element(By.XPATH, '//*[@id="iframe"]/div[1]/div[2]/iframe')
+iframe = driver.find_element(By.XPATH, '//div[@id="content"]/iframe')
 driver.switch_to.frame(iframe)
 time.sleep(2)
 
 #Print text
-elementText = driver.find_element(By.XPATH, "/html/head/title/")
+elementText = driver.find_element(By.XPATH, "/html/body/div/button")
 print(elementText.text)
 time.sleep(2)
 
@@ -26,5 +23,4 @@ driver.switch_to.default_content()
 driver.execute_script("window.scrollBy(0, 1000);")
 time.sleep(2)
 
-
-# '//iframe[contains(@src,"ejemplo_iframe.html")]'
+driver.quit()
