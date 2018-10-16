7c2dfa41b7e244e766ad249a057dc29b5acd41d4
Toni Robres <twiindan@gmail.com>
2018-10-16 12:46:20 +0200

    change folder name

--- Selenium/exercices/framework/core/base.py
+++
@@ -1,8 +0,0 @@
-class BasePage(object):
-    url = None
-
-    def __init__(self, driver):
-        self.driver = driver
-
-    def navigate(self):
-        self.driver.get(self.url)
--- test_automation/CSV/02_csv.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-csv_file = open("example.csv", "r")
-csv_reader = csv.reader(csv_file)
-for row in csv_reader:
-    print(row)
-
-csv_file.close()
