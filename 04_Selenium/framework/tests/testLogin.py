450d7d80fcecb597605030e4fd61f23f4730f591
Toni Robres <a.robres@scrm.edu>
2019-04-30 09:08:06 +0200

    delete logout

--- 04_Selenium/framework/tests/testLogin.py
+++ 04_Selenium/framework/tests/testLogin.py
@@ -18,23 +18,12 @@ class testLogin():
     def setup(self):
         self.login_page.navigate()
 
-
-    def test_incorrect_login(self):
-        self.login_page.locate_elements()
-        self.login_page.fillUsername('user2')
-        self.login_page.fillPassword('fakepassword')
-        self.login_page.submitClick()
-        errorUrl = self.login_page.verifyURL()
-        assert_equals('http://twiindan.pythonanywhere.com/admin/login/?next=/admin/', errorUrl)
-
-
     def test_correct_login(self):
         self.login_page.locate_elements()
         self.login_page.fillUsername('user1')
         self.login_page.fillPassword('selenium')
         self.login_page.submitClick()
-        header_page = headerPage(self.driver)
-        header_page.logout()
+
 
 
     @classmethod
